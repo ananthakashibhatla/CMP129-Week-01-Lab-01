16d96e3f85c5d483629f69472ac6b7d61e8e877c
AnanthaKashibhatla <ananthakashibhatla1@gmail.com>
2026-09-01 16:39:18 -0400

    completed both labs

--- BoxOffice.java
+++ BoxOffice.java
@@ -1,4 +1,35 @@
+import java.util.Scanner;
+
 public class BoxOffice {
-    
+    public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+        
+        System.out.println("What is the name of the movie? ");
+        String movieInput =  userInput.nextLine(); 
+
+        System.out.println("How many adult tickets were sold? ");
+        int adultTickets = userInput.nextInt(); 
+
+        System.out.println("How many child tickets were sold? ");
+        int childTickets = userInput.nextInt(); 
+
+        double adultProfits = 10.00 * adultTickets;
+        double childProfits = 6.00 * childTickets;
+
+        double totalProfits = adultProfits + childProfits;
+
+        double theaterProfits = 0.20 * totalProfits;
+
+        double distributorProfits = totalProfits - theaterProfits;
+
+        System.out.println("Movie Name: " + movieInput);
+        System.out.println("Adult Tickets Sold: " + adultTickets);
+        System.out.println("Child Tickets Sold: " + childTickets);
+        System.out.println("Gross Box Office Profit: " + totalProfits);
+        System.out.println("Net Box Office Profit: " + theaterProfits);
+        System.out.println("Amount Paid to Distributor: " + distributorProfits);
+
+    }
     
 }
--- InterestEarned.java
+++ InterestEarned.java
@@ -1,3 +1,28 @@
+import java.util.Scanner; 
+
 public class InterestEarned {
-    
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Please enter the principal amount: ");
+        double principalInput = userInput.nextDouble(); 
+
+        System.out.println("Please enter the annual interest rate: ");
+        double rateInput = userInput.nextDouble(); 
+
+        System.out.println("Please enter the number of times interest is compounded during the year");
+        int compoundTimesInput = userInput.nextInt(); 
+
+        double halfSolution = (1 + rateInput/compoundTimesInput);
+
+        double exponentialFragment = Math.pow(halfSolution, compoundTimesInput);
+
+        double finalSolution = principalInput * exponentialFragment;
+
+        System.out.println("Interest Rate: " + rateInput);
+        System.out.println("Times Compounded: " + compoundTimesInput);
+        System.out.println("Principal: " + principalInput);
+        System.out.println("Interest Earned: " + exponentialFragment);
+        System.out.println("Amount in Savings: " + finalSolution);
+    }
 }
